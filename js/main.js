986ce972a0d7e906bcc5eeebcfbadc84bb1cb9f4
mdnayan2025 <nayanmd412@gmail.com>
2025-09-29 18:31:15 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -19,14 +19,21 @@ $(document).ready(function () {
     }
   });
 
-  /* About me slider */
+  // Auto-close mobile menu on link click
+  $(".main-navigation__ul li a").on("click", function () {
+    $(".main-navigation__ul").removeClass("show");
+    $("#burger").removeClass("change").attr("aria-expanded", "false");
+    $("body").removeClass("stop-scroll");
+  });
+
+  /* About me slider - REMOVED DUPLICATE */
   $(".about-me-slider").slick({
     slidesToShow: 1,
     prevArrow: '<span class="span-arrow slick-prev"><</span>',
     nextArrow: '<span class="span-arrow slick-next">></span>',
   });
 
-  /* Blog slider */
+  /* Blog slider - REMOVED DUPLICATE */
   $(".blog-slider").slick({
     slidesToShow: 2,
     prevArrow: '<span class="span-arrow slick-prev"><</span>',
@@ -40,66 +47,36 @@ $(document).ready(function () {
       },
     ],
   });
-});
-
-	/* About me slider */
-	$('.about-me-slider').slick({
-		slidesToShow: 1,
-		prevArrow: '<span class="span-arrow slick-prev"><</span>',
-		nextArrow: '<span class="span-arrow slick-next">></span>'
-	});
-
-	/* Blog slider */
-	$('.blog-slider').slick({
-		slidesToShow: 2,
-		prevArrow: '<span class="span-arrow slick-prev"><</span>',
-		nextArrow: '<span class="span-arrow slick-next">></span>',
-		responsive: [
-		{
-			breakpoint: 768,
-			settings: {
-				slidesToShow: 1
-			}
-		}
-		]
-	});
-	
-});
-
-
+}); // <-- This closes the $(document).ready()
 
 var counta = 0;
 
 $(window).scroll(function(e){
-
-
-	/* Onscroll number counter */
-	var statisticNumbers = $('.single-count');
-	if(statisticNumbers.length) {
-		var oTop = statisticNumbers.offset().top - window.innerHeight;
-		if (counta == 0 && $(window).scrollTop() > oTop) {
-			$('.count').each(function() {
-				var $this = $(this),
-				countTo = $this.attr('data-count');
-				$({
-					countNum: $this.text()
-				}).animate({
-					countNum: countTo
-				},
-
-				{
-					duration: 2000,
-					easing: 'swing',
-					step: function() {
-						$this.text(Math.floor(this.countNum));
-					},
-					complete: function() {
-						$this.text(this.countNum);
-					}
-				});
-			});
-			counta = 1;
-		}
-	}
-
-});
+  /* Onscroll number counter */
+  var statisticNumbers = $('.single-count');
+  if(statisticNumbers.length) {
+    var oTop = statisticNumbers.offset().top - window.innerHeight;
+    if (counta == 0 && $(window).scrollTop() > oTop) {
+      $('.count').each(function() {
+        var $this = $(this),
+        countTo = $this.attr('data-count');
+        $({
+          countNum: $this.text()
+        }).animate({
+          countNum: countTo
+        },
+        {
+          duration: 2000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+          }
+        });
+      });
+      counta = 1;
+    }
+  }
+}); // <-- This closes the $(window).scroll() - WAS MISSING
